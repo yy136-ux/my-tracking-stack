b421099b33f3bd68b0e2c53774eca97be41d4226
yy136-ux <thereisnospoonyy136@gmail.com>
2026-09-17 22:05:53 +0800

    Update tracker.js

--- functions/tracker.js
+++ functions/tracker.js
@@ -251,7 +251,7 @@ async function sendToMeta({ body, clientIp, userAgent, fbp, fbc, hashedEm, hashe
       event_name: body.event_name,
       event_time: body.event_time,
       event_id: body.event_id,
-      event_source_url: body.event_source_url || '',
+      event_source_url: 'https://conxinch.com',
       action_source: 'website',
       user_data: metaUserData,
     }],
